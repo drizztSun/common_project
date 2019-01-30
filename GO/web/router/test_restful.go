4cc7feddb6913a83828e5d4d36110a0243e61561
drizztSun <freesssbird@gmail.com>
2019-01-30 17:03:05 -0500

    a restful for go

--- GO/web/router/test_restful.go
+++ GO/web/router/test_restful.go
@@ -8,16 +8,16 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// The person Type (more like an object)
 type Person struct {
-	ID        string
-	FristName string
-	LastName  string
-	Address   *Adress
+	ID        string   `json:"id,omitempty"`
+	Firstname string   `json:"firstname,omitempty"`
+	Lastname  string   `json:"lastname,omitempty"`
+	Address   *Address `json:"address,omitempty"`
 }
-
-type Adress struct {
-	City  string
-	State string
+type Address struct {
+	City  string `json:"city,omitempty"`
+	State string `json:"state,omitempty"`
 }
 
 //var people []Person
@@ -64,7 +64,9 @@ func (p *People) deletePerson(w http.ResponseWriter, r *http.Request) {
 func testRestfulAPI() {
 
 	r := mux.NewRouter()
-	p := People{}
+	p := People{[]Person{
+		Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}},
+		Person{ID: "2", Firstname: "Koko", Lastname: "Doe", Address: &Address{City: "City Z", State: "State Y"}}}}
 
 	r.HandleFunc("/people", p.getPeople).Methods("GET")
 	r.HandleFunc("/people/{id}", p.getPerson).Methods("GET")
