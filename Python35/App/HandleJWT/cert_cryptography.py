78ff46f221402cd30e501f05d7605ce46cbe92bf
YUXIN <yusun@akamai.com>
2019-09-04 15:38:58 -0400

    cer change

--- Python35/App/HandleJWT/cert_cryptography.py
+++ Python35/App/HandleJWT/cert_cryptography.py
@@ -3,6 +3,10 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import rsa
 
+import time
+from dateutil import parser
+from OpenSSL import crypto
+
 def output_cert_Info(cert):
     
     if not isinstance(cert, x509.Certificate):
@@ -40,6 +44,39 @@ def verify_cert_root_ca(cert, root_cert):
         None
     except Exception as e:
         print("Err " + e)
+
+def extract_public_key_from_certificate(stream):
+    """
+        extract public key from certificate, pem and der fromat
+    """
+    cert = crypto.load_certificate(crypto.FILETYPE_ASN1, stream)
+    return cert.get_pubkey().to_cryptography_key()#.decode("utf-8")
+
+def verify_dpc_signal_jws_token(request, signals):
+    #print("Headers : \n", request.headers)
+    print("Meta : \n", request.META)
+    print("COOKIE : \n", request.COOKIES)
+    #print("Request : \n", request)
+
+    #if 'HTTP_AUTHORIZATION' not in request.META:
+    #    print("No headers")
+    #    return False
+
+    #jws_token = request.META['HTTP_AUTHORIZATION'].lstrip()
+    
+    if 'COOKIE_NAME' not in request.COOKIES:
+        return False
+
+    jws_token = request.COOKIES['COOKIE_NAME'].lstrip()
+    #if not jws_token.startswith("Bearer "):
+    #    print("token format wrong : ", jws_token)
+    #    return False
+
+    #jws_token = jws_token[6:]
+    jws_token = jws_token.lstrip().rstrip()
+    device_id = signals['akamai_device_id']
+    return verify_jws_token(jws_token, {'sub': device_id})
+
 
 def main():
     None
