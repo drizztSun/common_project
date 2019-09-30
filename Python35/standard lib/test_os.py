11993de26d55c25f7a691cd8d25a306625e8895d
YUXIN <yusun@akamai.com>
2019-09-30 19:25:13 -0400

    Python

--- Python35/standard lib/test_os.py
+++ Python35/standard lib/test_os.py
@@ -11,15 +11,19 @@ def main():
     # to call outside command
     # 'system' to execute a command, like command < input | another command > output
     os.system("ipconfig")            
-    
+    print( os.system('ls -la') )
+
+
     # 'popen' can make a connection to std input and output
-    print(os.system("ls"))
     stream = os.popen("ipconfig")
+    print(stream.read())
+
+    stream = os.popen('ls -la')
     print(stream.read())
 
     # subprocss open
     # subprocess.run(["ls", "l", "//c/"])
-    print(subprocess.Popen("echo Hello world!", shell=True, stdout=subprocess.PIPE).stdout.read())
+    print(subprocess.Popen("echo Hello world-d''!", shell=True, stdout=subprocess.PIPE).stdout.read())
 
     # It will return, after the sub process complete
     print(subprocess.call("echo Hello world!", shell=True))
