374fff6a61f1121fd0ac9676077a3719b3c19576
YUXIN <yusun@akamai.com>
2019-10-01 17:02:55 -0400

    Python file

--- Python35/standard lib/files.py
+++ Python35/standard lib/files.py
@@ -1,4 +1,49 @@
+from os import listdir, system, path
+from os.path import isfile, join, isdir, exists
 import os
+
+targetRootFolder = ['inventory_app', 'version_app', 'zero_trust_inventory']
+
+skipfiles = set(
+['./inventory_app/test/test_portal_service.py',
+'./inventory_app/test/test_jwtutil.py',
+'./inventory_app/test/test_rule_engine.py',
+'./inventory_app/test/test_risk_assesment_api.py',
+'./inventory_app/test/test_cert_util.py',
+'./inventory_app/test/test_customer_rule_service.py',
+'__init__.py']
+)
+
+skipfolders = set(
+    ['test']
+)
+
+def generate_doc(target, dest):
+    param = ['pycco', target, '-d', dest]
+    print(system(' '.join(param)))
+
+
+def generate_doc_from_rootdir(rootdir, docdir):
+    for f in listdir(rootdir):
+        obj = join(rootdir, f)
+        if isfile(obj) and obj.endswith('.py') and obj not in skipfolders:
+            print(obj + ' : ' + docdir)
+            generate_doc(obj, docdir)
+        elif isdir(obj) and not f.startswith('.') and f not in skipfiles:
+            generate_doc_from_rootdir(obj, join(docdir, f))
+
+
+def scan_generate_doc():
+    for rootdir in targetRootFolder:
+        rootdir, docdir = join('./' + rootdir), join('./doc/' + rootdir)
+        if exists(rootdir):
+            generate_doc_from_rootdir(rootdir, docdir)
+
+
+if __name__ == '__main__':
+
+    scan_generate_doc()
+
 
 
 def ensure_dir(f):
