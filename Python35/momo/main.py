71ad5d45510ed83344aee3c86906c3e51c933429
yuxin <freesssbird@gmail.com>
2019-10-07 20:48:22 -0400

    Momo project

---
+++ Python35/momo/main.py
@@ -0,0 +1,21 @@
+
+
+
+
+
+i = 0
+while i < 10:  #incident
+    print("hello",i)
+    temp = input("guess")
+    guess = int(temp)
+    if guess == 7:                             
+        print("jackpot!!!!!")
+    else:
+        print("good luck next time!")
+        print("bee")   
+    i = i + 1
+
+
+
+
+    
