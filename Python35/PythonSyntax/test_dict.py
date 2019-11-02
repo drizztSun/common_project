8eeb5211d5087effcf6928149bf5a3015295ccc9
drizztsun <freesssbird@gmail.com>
2019-11-02 11:25:37 -0400

    upython

--- Python35/PythonSyntax/test_dict.py
+++ Python35/PythonSyntax/test_dict.py
@@ -1,3 +1,4 @@
+from copy import deepcopy
 
 
 def main():
@@ -12,6 +13,17 @@ def main():
     nums = {x : x**2 for x in range(6)}
     print(nums)
 
+    # *** access ***
+    print( "Peter: ", tel['Peter'])
+
+    print("Tom : ", tel.get('Tom', 100))
+    print("Jason : ", tel.get('Jason', 200)) # if key not in dict, it will return default-value
+
+    # getdefault
+    print( 'Jack : ', tel.get('Jack'))
+    print( 'Lucy : ', tel.get('Lucy', 'default-value')) # if key not in dict, it will return default-value
+
+    # *** add  and change***
     # update
     tel = {'Jack': 12, 'Peter': 13}
     tel.update(Tom=14)
@@ -22,7 +34,16 @@ def main():
     tel.update(tel1)
     print(tel)
 
+    # []
+    tel['Mozart'] = 100
+    tel['Chopin'] = 110
 
+    # setdefault
+    v = tel.setdefault("Tom", 100) # if key in dict, do nothing and return value
+    tel.setdefault("Mary", 200)
+    print( tel.setdefault('Dav', 1000)) # if key not in dict, it will add key, and then return default-value
+
+    # *** iter ***
     # iter
     for i in iter(tel):
         print("item : ", i, tel[i])
@@ -31,23 +52,16 @@ def main():
     for key in tel.keys():
         print(tel[key])
 
+    # items
+    print("items = ", tel.items())
+    for item in tel.items():
+        print(item[0], '->', item[1])
+
+
     # len
     print("len = ", len(tel))
 
-    # get
-    tel['Tom']
-
-    print("Tom : ", tel.get('Tom', 100))
-    print("Jason : ", tel.get('Jason', 200)) 
-
-    # set
-    tel['Tom'] = 12
-
-    # setdefault
-    v = tel.setdefault("Tom", 100)
-    tel.setdefault("Mary", 200)
-    print(tel)
-
+    # *** delete ***
     # del
     del tel['Tom']
     print('tel ', tel)
@@ -58,33 +72,50 @@ def main():
     assert( 'Tom' in tel)
 
     # pop
-    print("Tom ", tel.pop('Tom', 200))
-    print("Jeff ", tel.pop('Jeff', 300))
+    print("Tom ", tel.pop('Tom', 200)) # if key in dict, remove key-value and return value
+    print("Jeff ", tel.pop('Jeff', 300)) # if key not in dict, return default-value.
 
-    # copy
+    # popitem
+    while tel1:
+        print( "pop item ", tel1.popitem()) # remove last one in the dict 
+
+    # *** copy ***
+    # copy 
     tel2 = tel.copy()
-    print("tel ", tel)
-    print("tel2 ", tel2)
+    print("tel: ", tel)
+    print("tel2: ", tel2)
 
-    # items
-    print("items = ", tel.items())
-    for item in tel.items():
-        print(item[0], '->', item[1])
+    # deepcopy
+    tel3 = deepcopy(tel)
+    print("tel: ", tel),
+    print("tel3: ", tel3)
 
+    # *** keys and values ***
     # keys
     print("keys = ", tel.keys())
 
     # values
     print("values = ", tel.values())
 
+
+    # *** zero clear ***
     # clear
     tel.clear()
     print(tel)
 
-    # popitem
-    while tel1:
-        print("pop item ", tel1.popitem())
 
+# user-defined dict
+class mydict(dict):
+
+    def __getitem__(self, key):
+
+        return super().__getitem__(key)
+
+    def __setitem__(self, key, val):
+
+        return super().__setitem__(key, val)
+
+    
     
 
 if __name__ == '__main__':
