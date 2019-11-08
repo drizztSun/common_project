5faedd736508df14e3fd62324b1ead5c5592cb6e
YUXIN <yusun@akamai.com>
2019-11-08 12:31:00 -0500

    Python Pipenv environment

---
+++ Python35/algorithem/sort_search_alg.py
@@ -0,0 +1,29 @@
+
+
+def select_sort(a):
+    pass
+
+
+def insert_sort(a):
+    pass
+
+
+def bubble_sort(a):
+    pass
+
+
+def quick_sort(a):
+    pass
+
+
+def merge_sort(a):
+    pass
+
+
+def heap_sort(a):
+    pass
+
+
+if __name__ == '__main__':
+
+    select_sort([1, 7, 2, 6, 3, 5, 4])
