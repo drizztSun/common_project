b6f1a5f65c7278ce7a4fd94c03ff077bff12bf87
YUXIN <yusun@akamai.com>
2019-11-09 18:45:10 -0500

    Python mapping

--- Python35/PythonSyntax/collections/test_mapping.py
+++ Python35/PythonSyntax/collections/test_mapping.py
@@ -16,23 +16,43 @@ from collections.abc import Mapping, MutableMapping
 
 class Setting(Mapping):
 
+    def __init__(self, data=None):
+        pass
+        # self.update(data)
+
+    def __getitem__(self, key):
+        return self.get(key)
+
+    def __len__(self):
+        return len(self.keys())
+
+    def __iter__(self):
+        return self
+
+
+class MutSetting(MutableMapping):
+
     def __getitem__(self, key):
         pass
+
+    def __setitem__(self, key, val):
+        pass
+
+    def __delitem__(self, key):
+        pass
+
+    def __iter__(self):
+        return self
 
     def __len__(self):
         return 0
 
-    def __iter__(self):
-        pass
-
-
-class MutSetting(MutableMapping):
-    pass
-
 
 def test_setting():
 
-    pass
+    s = Setting(a=1, b=2, c=3)
+
+    print(s.get('key'))
 
 
 if __name__ == '__main__':
