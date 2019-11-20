542f9cc038f86cf5361eeb0fcd2dcc64837c0f75
yuxin <yuxin@yuxins-Mac-mini.hsd1.ma.comcast.net>
2019-11-19 21:46:16 -0500

    Momo update

--- Python35/momo/test_dict.py
+++ Python35/momo/test_dict.py
@@ -1,4 +1,4 @@
-
+print('========================================MATT============================================')
 
 # python test_dict.py
 # when call this command, test_dict will be the main module
@@ -20,6 +20,7 @@ if __name__ == "__main__":
 
     # add
     dict1['a'] = 9
+
     dict2.update(dict5)
     
     result = dict(dict5,**dict3) # use dictt5 and dict3 merge and get a new dict
@@ -27,10 +28,24 @@ if __name__ == "__main__":
     dict3.setdefault('n', 123456789)
 
     # delete
-    del dict2['a']
+    del dict2['a'] # Remove d[key] from d. Raises a KeyError if key is not in the map.
+
+
     assert('a' not in dict2)
-
-
-
-
     
+    dict5.clear()
+    
+    dict2.pop('b', [2]) # If key is in the dictionary, remove it and return its value, else return default. If default is not given and key is not in the dictionary, a KeyError is raised.
+
+    dict3.popitem() 
+    
+    # access
+    assert('n' in dict3)  #Return True if d has a key key, else False.
+
+    print("len : ", len(dict3))
+
+    #dict.cmp(dict4,dict3)
+    
+    print(dict3.items())
+    for k,v in dict3.items():
+        print(k,v)
