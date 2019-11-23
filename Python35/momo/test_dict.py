6420da61ddee36abfc482056962fd972f3630928
drizztsun <freesssbird@gmail.com>
2019-11-23 18:08:46 -0500

    Momo python

--- Python35/momo/test_dict.py
+++ Python35/momo/test_dict.py
@@ -1,9 +1,5 @@
-print('========================================MATT============================================')
 
-# python test_dict.py
-# when call this command, test_dict will be the main module
-if __name__ == "__main__":
-
+def test_dict()
     # create
     print("============TEST_DICT=================",)
     dict1 = {}
@@ -49,3 +45,72 @@ if __name__ == "__main__":
     print(dict3.items())
     for k,v in dict3.items():
         print(k,v)
+
+def counter_list(a):
+    counter_dict = {}
+
+    for i in range(len(a)):
+        if a[i] not in counter_dict:
+            counter_dict[a[i]] = 1
+        else:
+            counter_dict[a[i]] +=1
+
+    for k, v in counter_dict.items():
+        print("key {} occurs {} times.".format(k, v))    
+
+
+
+
+if __name__ == "__main__":
+    
+    
+    a = [1, 23, 2, 7, 4, 5, 10, 2, 7, 6, 5, 2, 1, 4, 2, 2, 2]
+
+    counter_list(a)
+    
+    dect = {'a':10,'b':20,'c':30}
+
+    print(dect.get('a'))
+
+    #create
+    dect1 = {}#a clear one
+
+    dect2 = {'a':100}# a classtic way.:D
+
+    dect3 = dict(a= 100)
+    
+    dect4 = dect1 # 
+    assert(id(dect4) == id(dect1))
+    
+    dect5 = dect2.copy()  # copy a dict
+    assert(id(dect5) != id(dect2))
+
+    # exist
+    assert( 'a' in dect1)
+    assert( 'sdfjskfds' not in dect1)
+
+    # iterate
+    for k in dect1:
+        print('key {} occurs {} times '.format(k))
+
+    # add
+    b = 'b'
+    dect1[b] = 200
+    
+    dect1.update({'xy': 1000, 'xz': 10000}) # put new key-value into dect1, and update key-value
+    print(dect1)
+
+    result = dict(dect1,**dect2)
+    print(result)
+
+    dect5.setdefault('c',123456789) # set 'c':123456, if 'c' does not exist, return default value(none)
+
+    # delete
+    del dect1['xy']
+    dect2.clear() # clear dect2,dect2 = {} dect2 still exists
+    print (dect5.pop('c'))
+    print (dect3.popitem())
+    del(dect4) #clear and delate dect4 ,dect4 now don't exist
+
+    # change
+    dect1['xz'] = 99999 #make it to a any new value, like int,float,str
