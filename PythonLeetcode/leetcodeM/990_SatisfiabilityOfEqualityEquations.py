dd41c211f8c5926f8b804603f95799349d518ccc
YUXIN <yusun@akamai.com>
2020-02-10 15:43:26 -0500

    Python update

--- PythonLeetcode/leetcodeM/990_SatisfiabilityOfEqualityEquations.py
+++ PythonLeetcode/leetcodeM/990_SatisfiabilityOfEqualityEquations.py
@@ -7,7 +7,6 @@
 # Return true if and only if it is possible to assign integers to variable names so as to satisfy all the given equations.
 
 
-
 # Example 1:
 
 # Input: ["a==b","b!=a"]
@@ -47,20 +46,76 @@
 
 class EquationsPossible:
 
+    """
+    Approach 1: Connected Components
+    Intuition
+
+    All variables that are equal to each other form connected components.
+    For example, if a=b, b=c, c=d then a, b, c, d are in the same connected component as they all must be equal to each other.
+
+    Algorithm
+
+    First, we use a depth first search to color each variable by connected component based on these equality equations.
+
+    After coloring these components, we can parse statements of the form a != b.
+    If two components have the same color, then they must be equal, so if we say they can't be equal then it is impossible to satisfy the equations.
+
+    Otherwise, our coloring demonstrates a way to satisfy the equations, and thus the result is true.
+
+    Complexity Analysis
+
+    Time Complexity: O(N) where N is the length of equations.
+
+    Space Complexity: O(1), assuming the size of the alphabet is O(1).
+    """
+
     def doit(self, equations):
-        pass
+
+        graph = [[] for _ in range(26)]
+
+        for eqn in equations:
+            if eqn[1] == '=':
+                x = ord(eqn[0]) - ord('a')
+                y = ord(eqn[3]) - ord('a')
+                graph[x].append(y)
+                graph[y].append(x)
+
+        color = [None] * 26
+        t = 0
+        for start in range(26):
+            if color[start] is None:
+                t += 1
+                stack = [start]
+                while stack:
+                    node = stack.pop()
+                    for nei in graph[node]:
+                        if color[nei] is None:
+                            color[nei] = t
+                            stack.append(nei)
+
+        for eqn in equations:
+            if eqn[1] == '!':
+                x = ord(eqn[0]) - ord('a')
+                y = ord(eqn[3]) - ord('a')
+                if x == y:
+                    return False  # lee
+                if color[x] is not None and color[x] == color[y]:
+                    return False
+        return True
 
 
 if __name__ == '__main__':
 
-    res = EquationsPossible().doit(["a==b","b!=a"])
+    res = EquationsPossible().doit(["a==b", "b!=a"])
 
-    res = EquationsPossible().doit(["b==a","a==b"])
+    res = EquationsPossible().doit(["b==a", "a==b"])
 
-    res = EquationsPossible().doit(["a==b","b==c","a==c"])
+    res = EquationsPossible().doit(["a==b", "b==c", "a==c"])
 
-    res = EquationsPossible().doit(["a==b","b!=c","c==a"])
+    res = EquationsPossible().doit(["a==b", "b!=c", "c==a"])
 
-    res = EquationsPossible().doit(["c==c","b==d","x!=z"])
+    res = EquationsPossible().doit(["c==c", "b==d", "x!=z"])
+
+    res = EquationsPossible().doit(["e==e", "d!=e", "c==d", "d!=e"])
 
     pass
---
+++ PythonLeetcode/leetcodeM/991_BrokenCalculator.py
@@ -0,0 +1,93 @@
+# 991. Broken Calculator
+
+# On a broken calculator that has a number showing on its display, we can perform two operations:
+
+# Double: Multiply the number on the display by 2, or;
+# Decrement: Subtract 1 from the number on the display.
+# Initially, the calculator is displaying the number X.
+
+# Return the minimum number of operations needed to display the number Y.
+
+
+# Example 1:
+
+# Input: X = 2, Y = 3
+# Output: 2
+# Explanation: Use double operation and then decrement operation {2 -> 4 -> 3}.
+# Example 2:
+
+# Input: X = 5, Y = 8
+# Output: 2
+# Explanation: Use decrement and then double {5 -> 4 -> 8}.
+# Example 3:
+
+# Input: X = 3, Y = 10
+# Output: 3
+# Explanation:  Use double, decrement and double {3 -> 6 -> 5 -> 10}.
+
+# Example 4:
+
+# Input: X = 1024, Y = 1
+# Output: 1023
+# Explanation: Use decrement operations 1023 times.
+
+
+# Note:
+
+# 1 <= X <= 10^9
+# 1 <= Y <= 10^9
+
+class BrokenCalc:
+
+    """
+    Approach 1: Work Backwards
+    Intuition
+
+    Instead of multiplying by 2 or subtracting 1 from X, we could divide by 2 (when Y is even) or add 1 to Y.
+
+    The motivation for this is that it turns out we always greedily divide by 2:
+
+    If say Y is even, then if we perform 2 additions and one division,
+    we could instead perform one division and one addition for less operations [(Y+2) / 2 vs Y/2 + 1].
+
+    If say Y is odd, then if we perform 3 additions and one division, we could instead perform 1 addition,
+    1 division, and 1 addition for less operations [(Y+3) / 2 vs (Y+1) / 2 + 1].
+
+    Algorithm
+
+    While Y is larger than X, add 1 if it is odd, else divide by 2. After, we need to do X - Y additions to reach X.
+
+    Complexity Analysis
+
+    Time Complexity: O(logY).
+
+    Space Complexity: O(1).
+    """
+
+    def doit(self, X, Y):
+
+        ans = 0
+
+        while Y > X:
+
+            ans += 1
+
+            if Y % 2:
+                Y += 1
+            else:
+                Y //= 2
+
+        return ans + X - Y
+
+
+if __name__ == '__main__':
+
+    res = BrokenCalc().doit(X=2, Y=3)
+
+    res = BrokenCalc().doit(X=5, Y=8)
+
+    res = BrokenCalc().doit(X=3, Y=10)
+
+    res = BrokenCalc().doit(X=1024, Y=1)
+
+    pass
