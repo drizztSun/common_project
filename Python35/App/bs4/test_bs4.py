fbd39ed5229eda13803bfd19bc1db232a21c61c9
YUXIN <yusun@akamai.com>
2020-04-27 20:40:25 -0400

    Python update

--- Python35/App/bs4/test_bs4.py
+++ Python35/App/bs4/test_bs4.py
@@ -1,11 +1,39 @@
-
-
 from bs4 import BeautifulSoup
 import requests
 import re
+import datetime
+import dateutil
+import urllib.parse
 
 
-def test_bs4_fetch():
+def get_version(x):
+    """
+
+    :param x: version string, such as 1.1.1
+    :return: tuple of integer
+    """
+
+    if isinstance(x, str):
+        s = []
+        for a in x.split('.'):
+            a = a.strip()
+            if a:
+                try:
+                    s.append(int(a))
+                except ValueError:
+                    s.append(a)
+
+        # remove all trailing zeros
+        if isinstance(s, list):
+            while s and not s[-1]:
+                s.pop(-1)
+
+        return tuple(s)
+
+    return x
+
+
+def test_bs4_fetch_ubuntu():
 
     heads = {
         "Accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
@@ -48,76 +76,161 @@ def test_bs4_fetch():
         print(f'{version}, {code_name}, {docs}, {release_date}, {end_of_support}, {end_of_life}, {version_num}')
 
 
-def test_fetch_ubuntu():
-
-    url = "https://wiki.ubuntu.com/Releases"
-
-    res = requests.get(url)
-    text = res.text
-
-    doc_root = BeautifulSoup(res.text, 'lxml')
-
-
-    version = "18.04"
-    re_ver = re.compile(r'^(?P<version>' + version + r'\\.\\d)$')
-
-    tds = doc_root.find_all('td')
-    for td in tds:
-        m = re.search(re_ver, td.get_text())
-        if m:
-            print(m.groupdict())
-
-
-
-
 def test_bs4_fetch_macos():
 
     url = "https://developer.apple.com/news/releases/rss/releases.rss"
+    version_re =  r'^10.12'
+    build_re = r'^16[A-Z]'
+    date_re = r'[a-zA-Z]+\s+\d{1,2}\s*,\s*\d{4}'
 
     res = requests.get(url)
+    res.raise_for_status()
 
-    doc_root = BeautifulSoup(res.text, 'lxml')
+    root = BeautifulSoup(res.text, 'lxml')
+    versions = []
 
-    items = doc_root.find_all('item')
+    for n in root.find_all('table'):
+        version = n.find('th', string=re.compile(r'Version'))
+        build = n.find(['th', 'span'], string=re.compile(r'Build'))
+        tbody = n.find('tbody')
+        if version and build and tbody:
+            version, build, date = None, None, None
+            for tr in tbody.find_all('tr'):
+                if version and build:
+                    versions.append((version, build, date))
 
-    item = doc_root.find('item')
+                version_th = tr.find('th', string=re.compile(version_re))
+                if version_th:
+                    version = version_th.get_text().strip()
 
-    pass
+                build_th = tr.find('td', string=re.compile(build_re))
+                if build_th:
+                    build = build_th.get_text().strip()
+
+                for td in tr.find_all('td'):
+                    m = re.match(date_re, td.get_text())
+                    if m:
+                        date = str(datetime.datetime.strptime(m.group(0).strip(), '%B %d, %Y').date())
+
+    print(',\n'.join(str(v) for v in versions))
 
 
 def test_bs4_fetch_firefox():
 
-    url = 'http://ftp.mozilla.org/pub/firefox/releases/'
-
+    url = 'https://ftp.mozilla.org/pub/firefox/releases/'
     res = requests.get(url)
+    res.raise_for_status()
+
+    root = BeautifulSoup(res.text, 'lxml')
+    versions = []
+
+    age = datetime.date() - datetime.timedelta(days=365*2)
+
+    url_parsed = urllib.parse.urlparse(url)
+
+    for n in root.find_all('a', string=re.compile(r'^([0-9]+[0-9.]+)/')):
+
+        version = re.match(r'^([0-9]+[0-9.]+)/', n.get_text().strip()).group(1)
+
+        url = '%s://%s/%s' % (url_parsed.scheme, url_parsed.netloc, n.get('href'))
+
+        res = requests.get(url)
+        res.raise_for_status()
+
+        node = BeautifulSoup(res.text, 'lxml')
+
+        date_td = node.find('td', string=re.compile(r'\d{1,2}-[a-zA-Z]*-\d{4} \d+:\d+$'))
+        if date_td:
+            date = datetime.datetime.strptime(date_td.get_text().strip(), '%d-%b-%Y %H:%M').date()
+            if date > age:
+                if 'win' in node.get_text():
+                    versions.append(('windows', version, date.isoformat()))
+                    print('windows', version, date)
+
+                if 'mac' in node.get_text():
+                    versions.append(('darwin', version, date.isoformat()))
+                    print('mac', version, date)
+
+    versions.sort(key=lambda x: get_version(x[1]))
+    print(',\n'.join(str(v) for v in versions))
+
+
+def test_bs4_fetch_windows():
+
+    version, build = 1903, 18362
+
+    res = requests.get('https://winreleaseinfoprod.blob.core.windows.net/winreleaseinfoprod/en-US.html')
+    res.raise_for_status()
 
     doc_root = BeautifulSoup(res.text, 'lxml')
 
-    items = doc_root.find_all(name='a')
+    build_re = re.compile(r'^%d.\d+' % build)
+    versions = []
 
-    print(items.__dir__)
+    for table in doc_root.find_all('table'):
 
-    re_text = re.compile(r'^(?P<version>[\d]+[\d\.]*)/$')
-    nodes = []
+        build = table.find('td', string=re.compile(build_re))
 
-    for n in items:
+        if build and not table.find('td', string=re.compile(version)):
 
-        res = re.search(re_text, n.get_text())
+            for tr in table.find_all('tr'):
 
-        if res:
-            d = res.groupdict()
-            d['node'] = n
-            nodes.append(d)
+                build_th = tr.find('td', string=re.compile(build_re))
 
-    print(nodes)
-    pass
+                if build_th:
+
+                    build = build_th.get_text().strip()
+
+                    date_th = tr.find('td', string=re.compile(r'^\d{4}-\d{2}-\d{2}'))
+
+                    if date_th:
+                        date = str(datetime.datetime.strptime(date_th.get_text().strip(), '%Y-%m-%d').date())
+                        versions.append((version, build, date))
+
+    print(',\n'.join(str(v) for v in versions))
+
+
+def test_bs4_fetch_chrome(self, *args, **options):
+
+    url = 'https://chromereleases.googleblog.com/{year}/{month:02d}/stable-channel-update-for-desktop.html' # 'https://chromereleases.googleblog.com'
+
+    today = datetime.date.today().replace(day=1)
+    start = today - datetime.timedelta(days=365*2)
+
+    versions = []
+    while start <= today:
+
+        url = url.format(year=start.year, month=start.month)
+        start += dateutil.relativedelta.relativedelta(months=1)
+
+        res = requests.get(url)
+        res.raise_for_status()
+
+        root = BeautifulSoup(res.text, 'lxml')
+
+        for post in root.find_all('div', class_='post'):
+            date = post.find('span', class_='publishdate').get_text().strip()
+            if date:
+                date = str(datetime.datetime.strptime(date, '%A, %B %d, %Y').date())
+                body = post.find('div', class_='post-body')
+                if body:
+                    for span in body.find_all('span'):
+                        m = re.search(r'Chrome\s+([0-9]+[0-9.]+)\s+', span.get_text())
+                        if m:
+                            version = m.group(1)
+                            versions.append(('windows', version, date))
+                            versions.append(('darwin', version, date))
+                            # print(version, date)
+
+    versions.sort(key=lambda x: get_version(x[1]))
+    print(',\n'.join(str(v) for v in versions))
 
 
 if __name__ == '__main__':
 
     # test_bs4_fetch()
 
-    test_fetch_ubuntu()
+    test_bs4_fetch_ubuntu()
 
     test_bs4_fetch_firefox()
 
