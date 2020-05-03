9bce4d82925eb14d3d4c3d2c48b1b59a787d4674
yuxin <freesssbird@gmail.com>
2020-05-03 08:41:51 -0400

    momo

--- cppConsole/cppConsole.cpp
+++ cppConsole/cppConsole.cpp
@@ -59,9 +59,104 @@ void select_sort(int* a, int length) {
 	}
 }
 
+
+struct student_grade{
+	char name[20];
+	int stu_code;
+
+	int grade;
+	int eglish_grade;
+	char grade2;
+	char eglish_grade2;
+	bool failed_or_not;
+	bool eg_failed_or_not;
+};
+
+// global variable
+student_grade stua = { "owen", 20150402391, 97.5, 100, 'A', 'A', false, true };
+
+struct point {
+	int x, y;
+};
+
+
+void test_point() {
+
+	//1:members of a structure can be accessed by the"."operator.
+	//ex:
+	printf("1:members of a structure can be accessed by the dot(.) operator.\nex:\n");
+	struct point point1 = { 2, 4 };//{x, y}
+	printf("now:x:%d y:%d\n", point1.x, point1.y);
+	point1.x = 10;
+	printf("now:x:%d y:%d\n", point1.x, point1.y);
+	point1.y = 20;
+	printf("now:x:%d y:%d\n\n", point1.x, point1.y);
+
+	//2:You can create a array with structures. 
+    //ex:
+
+	printf("2:You can create a array with structures.\nex:\n");
+	struct point aop[10];
+	aop[0].x = 10;
+	aop[0].y = 20;
+	printf("output: x:%d y:%d \n\n", aop[0].x, aop[0].y);
+
+	//3:You can use a pointer for a class , if we have a pointer to a structure, members 
+	//of a structure can be accessed by "->"operator(It looks like a pointer).
+    //ex:
+	printf("3:You can use a pointer for a class , if we have a pointer to a structure, members of a "); 
+	printf("structure can be accessed by pointer(->) operator(It looks like a pointer).\nex:\n");
+
+	struct point p1 = { 1, 2 };
+
+	struct point *p2 = &p1;
+
+	printf("p2: x: %d y:%d\n\n", p2->x, p2->y);
+
+	
+
+}
+
+void test_bit_operator() {
+
+	// '<<' '>>' '&', '|' '^'
+
+
+	int i = 1;
+
+	i <<= 2; // left-move 2 bit, becoming  0100, is equal to i*2*2
+
+	i >>= 1; // right-move 1 bit, becoming 0010, equal to i//2
+
+	int j = 1 & 0;
+
+	
+
+
+
+
+	return;
+}
+
+
+
+
+
 int main() {
 
+	test_bit_operator();
+
+	// static variable 
+	static int vc = 100;
+
+	vc = 250;
+
+	//local variable
 	int a = 90;
+	
+	unsigned int ua = -100;
+	printf(" ua %u \n", ua);
+
 	float b = 3.1416;
 
 	double c = 3.14;
@@ -108,7 +203,33 @@ int main() {
 	int o[5] = { 1, 3, 2, 5, 4};
 	select_sort(o, 5);
 
+	{
+		int num;
+
+		num = 1;
+		
+		num = 3;
+
+		printf("%d \n", num++); // get value, do +1
+		// do(i)
+		// i++
+
+		printf("%d \n", num);
+
+		printf("%d \n \n\n\n\n", ++num); // do +1, get value
+		// i += 1
+		// do(i)
+	}
+
+
+	test_point();
+	
 	return 1;
 
 }
+
+
+
+
+
 
