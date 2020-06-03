eb94179602e5e428f7fef6b96fbb84e0436ea231
YUXIN <yusun@akamai.com>
2020-06-03 10:34:58 -0400

    Rust create

--- Rust/libtest/src/main.rs
+++ Rust/libtest/src/main.rs
@@ -2,6 +2,8 @@ use crate::List2::{Cons2, Nil};
 use std::cell::RefCell;
 use std::rc::Rc;
 
+extern crate rary;
+
 #[derive(Debug)]
 enum List2 {
     Cons2(i32, RefCell<Rc<List2>>),
@@ -42,7 +44,34 @@ pub fn test_reference_cycle() {
     // println!("a next item = {:?}", a.tail());
 }
 
+/*
+
+    *** extern crate ***
+To link a crate to this new library, the extern crate declaration must be used.
+This will not only link the library, but also import all its items under a module named the same as the library. The visibility rules that apply to modules also apply to libraries.
+
+
+# Where library.rlib is the path to the compiled library, assumed that it's
+# in the same directory here:
+$ rustc main.rs --extern rary=library.rlib && ./main
+called rary's `public_function()`
+called rary's `indirect_access()`, that
+> called rary's `private_function()`
+
+*/
+fn test_rary() {
+
+    rary::public_function();
+
+    // Error! `private_function` is private
+    //rary::private_function();
+
+    rary::indirect_access();
+}
+
 
 fn main() {
     test_reference_cycle();
+
+    test_rary();
 }
