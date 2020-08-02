9672afc7bb7ee740e758d030d737a2d1e24fc882
drizztsun <freesssbird@gmail.com>
2020-08-02 14:36:14 -0400

    momo c

---
+++ Python35/momo/C/TestC/TestC/testclink.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+typedef struct node {
+    int val;
+    struct node* next;
+} node_t;
+
+
+int test_clink() {
+
+    node_t* head = (node_t*)malloc(sizeof(node_t));;
+    if (head == NULL) {
+        return 1;
+    }
+
+    head->val = 1;
+    head->next = NULL;
+
+    //We've just created the first variable in the list. We must set the value, and the next item to be empty, if we want to finish populating the list. Notice that we should always check if malloc returned a NULL value or not.
+
+    //To add a variable to the end of the list, we can just continue advancing to the next pointer :
+
+    node_t* head = NULL;
+    head = (node_t*)malloc(sizeof(node_t));
+    head->val = 1;
+    head->next = (node_t*)malloc(sizeof(node_t));
+    head->next->val = 2;
+    head->next->next = NULL;
+}
+
