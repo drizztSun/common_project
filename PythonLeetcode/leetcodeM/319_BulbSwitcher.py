2b8e9eb1472e975cb2e74b0e07d77508028567ae
drizztsun <freesssbird@gmail.com>
2021-01-26 12:42:20 -0500

    leetcode

--- PythonLeetcode/leetcodeM/319_BulbSwitcher.py
+++ PythonLeetcode/leetcodeM/319_BulbSwitcher.py
@@ -39,6 +39,36 @@ Constraints:
 
 class BulbSwitch:
 
+    """
+        The sqrt(n) solution can be explained using following matrix.
+
+        1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,
+        2,4,6,8,10,12,14,16,18,20,
+        3,6,9,12,15,18,
+        4,8,12,16,20,
+        5,10,15,20,
+        6,12,18,
+        7,14,
+        8,16,
+        9,18,
+        10,20,
+        11,
+        12,
+        13,
+        14,
+        15,
+        16,
+        17,
+        18,
+        19,
+        20,
+        Each line in this matrix indicates which bulb should be switched. This is a Symmetric Matrices, so only the diagonal elements will be visited odd. The non-diagonal elements will be visited twice. 
+        So only the diagonal elements will be "true" after all. The length of the diagonal elements is sqrt(n). The number of "true" also will be the length of the diagonal elements.
+    """
+    def doit_(self, n: int) -> int:
+        from math import sqrt
+        return int(sqrt(n))
+
     def doit_(self, n: int) -> int:
         
         count = 0
