39c88f3e42a29e32f935a32ff4e51f5bf888e8ea
drizztsun <freesssbird@gmail.com>
2021-04-20 14:25:56 -0400

    leetcode

--- cppLeetcode/leetcodeM/873_LengthOfLongestFibonacciSubsequence.cpp
+++ cppLeetcode/leetcodeM/873_LengthOfLongestFibonacciSubsequence.cpp
@@ -42,7 +42,35 @@ class LongestFibSubseq {
 
 public:
 
-    int doit_(vector<int>& arr) {
+    /*
+        Approach 2: Dynamic Programming
+        Intuition
+
+        Think of two consecutive terms A[i], A[j] in a fibonacci-like subsequence as a single node (i, j), and the entire subsequence is a path between these consecutive nodes. For example, with the fibonacci-like subsequence (A[1] = 2, A[2] = 3, A[4] = 5, A[7] = 8, A[10] = 13), we have the path between nodes (1, 2) <-> (2, 4) <-> (4, 7) <-> (7, 10).
+
+        The motivation for this is that two nodes (i, j) and (j, k) are connected if and only if A[i] + A[j] == A[k], and we needed this amount of information to know about this connection. Now we have a problem similar to Longest Increasing Subsequence.
+
+        Algorithm
+
+        Let longest[i, j] be the longest path ending in [i, j]. Then longest[j, k] = longest[i, j] + 1 if (i, j) and (j, k) are connected. Since i is uniquely determined as A.index(A[k] - A[j]), this is efficient: we check for each j < k what i is potentially, and update longest[j, k] accordingly.
+
+
+        Complexity Analysis
+
+        Time Complexity: O(N^2)O(N 
+        2
+        ), where NN is the length of A.
+
+        Space Complexity: O(N \log M)O(NlogM), where MM is the largest element of A. We can show that the number of elements in a subsequence is bounded by O(\log \frac{M}{a})O(log 
+        a
+        M
+        ​	
+        ) where aa is the minimum element in the subsequence.
+
+    
+    */
+
+    int doit_dp_best(vector<int>& arr) {
         
         int N = arr.size();
         unordered_map<int, int> index;
@@ -60,13 +88,13 @@ public:
 
                     int i = index[arr[k] - arr[j]];
 
-                    longest[k*N+j] = longest[j*N+i] + 1;
-                    ans = std::max(ans, longest[k*N+j]);
+                    longest[j*N+k] = longest[i*N+j] + 1;
+                    ans = std::max(ans, longest[j*N+k]+2);
                 }
             }
 
         }
         
-        return ans > 2 ? 0 : ans;
+        return ans > 2 ? ans : 0;
     }
 };
